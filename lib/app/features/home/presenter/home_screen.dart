ddb65eecacd731070077411d4d51c0202f608337
Kevin-Sarges <kevinsarges08@gmail.com>
2023-03-06 15:53:02 -0300

    home

---
+++ lib/app/features/home/presenter/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:connect_pets/app/common/utils/colors_app.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: ColorsApp.lightGray,
+    );
+  }
+}
