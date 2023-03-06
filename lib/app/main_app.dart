329151d52722da22e54feba3299d4dc49b3f8e64
Kevin-Sarges <kevinsarges08@gmail.com>
2023-03-06 14:06:45 -0300

    banner debug

--- lib/app/main_app.dart
+++ lib/app/main_app.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Connect pet',
+      debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
   }
