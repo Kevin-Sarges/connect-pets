b0357df6428bbbee03799694792dbe6c6e1911e9
Kevin-Sarges <kevinsarges08@gmail.com>
2023-03-06 16:04:38 -0300

    doar

---
+++ lib/app/features/doar/presenter/doar_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DoarScreen extends StatefulWidget {
+  const DoarScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DoarScreen> createState() => _DoarScreenState();
+}
+
+class _DoarScreenState extends State<DoarScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
