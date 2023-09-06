830ca07a2c57fc5d3b383326529c3f226f3d70b2
Kevin-Sarges <kevinsarges08@gmail.com>
2023-09-05 22:00:15 -0300

    refactor: ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:connect_pets/app/common/inject/inject_dependecy.dart';
+import 'package:connect_pets/app/common/inject/inject_dependency.dart';
 import 'package:connect_pets/app/main_app.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
