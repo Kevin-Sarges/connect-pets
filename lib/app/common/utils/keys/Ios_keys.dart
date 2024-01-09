94b293d7104727c31de866464092ca6292a1b4d4
Kevin-Sarges <kevinsarges08@gmail.com>
2024-01-09 19:23:13 -0300

    refactor: novas chaves

---
+++ lib/app/common/utils/keys/Ios_keys.dart
@@ -0,0 +1,10 @@
+class IOSKeys {
+  static const apiKey = String.fromEnvironment('API_KEY_IOS');
+  static const appId = String.fromEnvironment('APP_ID_IOS');
+  static const messagingSenderId =
+  String.fromEnvironment('MESSAGING_SENDER_ID_IOS');
+  static const projectId = String.fromEnvironment('PROJECT_ID_IOS');
+  static const storageBucket = String.fromEnvironment('STORAGE_BUCKET_IOS');
+  static const iosClientId = String.fromEnvironment('CLIENT_ID_IOS');
+  static const iosBundleId = String.fromEnvironment('BUNDLE_ID_IOS');
+}
---
+++ lib/app/common/utils/keys/android_keys.dart
@@ -0,0 +1,8 @@
+class AndroidKeys {
+  static const apiKey = String.fromEnvironment('API_KEY_ANDROID');
+  static const appId = String.fromEnvironment('APP_ID_ANDROID');
+  static const messagingSenderId =
+  String.fromEnvironment('MESSAGING_SENDER_ID_ANDROID');
+  static const projectId = String.fromEnvironment('PROJECT_ID_ANDROID');
+  static const storageBucket = String.fromEnvironment('STORAGE_BUCKET_ANDROID');
+}
