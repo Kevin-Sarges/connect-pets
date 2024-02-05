69f6ad94d2cabf87e209cf85dbbd156797331bfc
Kevin-Sarges <kevinsarges08@gmail.com>
2024-02-05 16:52:38 -0300

    feat: nova imagem

--- lib/app/common/utils/images_app.dart
+++ lib/app/common/utils/images_app.dart
@@ -3,4 +3,5 @@ class ImagesApp {
   static const String google = 'assets/images/google_icon.png';
   static const String facebook = 'assets/images/facebook_icon.png';
   static const String apple = 'assets/images/apple_icon.png';
+  static const String listaVazia = 'assets/images/cachorro.png';
 }
