72d59c4881db4a8c2c466108ffa20a45f1079e1f
Kevin-Sarges <kevinsarges08@gmail.com>
2024-02-08 09:58:20 -0300

    refactor: nova arquitetura

--- lib/app/common/inject/inject_dependency.dart
+++ lib/app/common/inject/inject_dependency.dart
@@ -1,10 +1,10 @@
 import 'package:connect_pets/app/features/feed/utils/inject/feed_inject_dependency.dart';
-import 'package:connect_pets/app/features/finish_signup/domain/inject/finish_inject_dependency.dart';
-import 'package:connect_pets/app/features/home/domain/inject/home_inject_dependency.dart';
-import 'package:connect_pets/app/features/login/domain/inject/login_inject_dependency.dart';
-import 'package:connect_pets/app/features/refactor_password/domain/inject/refactor_inject_dependency.dart';
-import 'package:connect_pets/app/features/signup/domain/inject/signup_inject_dependency.dart';
-import 'package:connect_pets/app/features/splash/domain/inject/splash_inject.dart';
+import 'package:connect_pets/app/features/finish_signup/utils/inject/finish_inject_dependency.dart';
+import 'package:connect_pets/app/features/home/utils/inject/home_inject_dependency.dart';
+import 'package:connect_pets/app/features/login/utils/inject/login_inject_dependency.dart';
+import 'package:connect_pets/app/features/refactor_password/utils/inject/refactor_inject_dependency.dart';
+import 'package:connect_pets/app/features/signup/utils/inject/signup_inject_dependency.dart';
+import 'package:connect_pets/app/features/splash/utils/inject/splash_inject.dart';
 import 'package:get_it/get_it.dart';
 
 class InjectDependency {
